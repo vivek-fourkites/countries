1fe74c7f11846506d5129c03521b16eec051401e
vivek-fourkites <38906143+vivek-fourkites@users.noreply.github.com>
2018-05-14 16:00:49 +0530

    Wrapper for accessing more methods  (#1)
    * Squashed commit of the following: Wrapper class for additional methods
    
    commit 8b1e7e8c602f875ce995e3cec874c25200755b57
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 15:40:55 2018 +0530
    
        Added support for translation match
    
    commit 6cc351e4b8c5a1616a0b13ffc5cfd5dc3607ba87
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 14:54:58 2018 +0530
    
        test cases
    
    commit 97b0719476d5ca324a06cf48a954a74938686b43
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 13:17:49 2018 +0530
    
        Added some test cases
    
    commit 9a7c7cf66ac0a53386fdff86cfdeeeee407b29f0
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 13:08:56 2018 +0530
    
        Adding the logic for unofficial_names
    
    commit 491e6ce94b9867ec2d9b63f64fd6692954a818f6
    Merge: 4bc4278 af9b1a5
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 12:50:11 2018 +0530
    
        Merge branch 'master' of https://github.com/vivek-fourkites/countries
    
    commit 4bc427826cb57e7bd201c06e8857ad64c0db4146
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 12:40:17 2018 +0530
    
        changing the logic of the method
    
    commit ed4007c58ab43252dc075a1e223e099d5d560ff9
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 12:22:21 2018 +0530
    
        initial commit missed file
    
    commit 4c79dabc19692dfe48077e30982a605ad16e5929
    Author: Vivek N <vivek.nagaraju@fourkites.com>
    Date:   Mon May 14 12:21:58 2018 +0530
    
        Initial commit for fourkites country gem
    
    * test cases
    
    * Indendation

---
+++ lib/countries/fourkites_country.rb
@@ -0,0 +1,67 @@
+
+		$: << 'lib'
+		require_relative 'iso3166'
+		require 'pry'
+
+		module FourKites
+			class Country 
+				class << self
+				def is_state_valid? state_code , country_code
+					return false if state_code.empty? || country_code.empty?
+		            country = ISO3166::Country.new(country_code)
+		            return true if country.states.include?(state_code.upcase)
+		            false
+				end
+
+				def get_correct_state_code state_name , country_code
+					 return nil if state_name.empty? || country_code.empty?
+		             country = ISO3166::Country.new(country_code)
+		             max_count = 0
+		             final_key = ""
+		             translated_key = is_translation_match(state_name , country_code)
+		             return translated_key unless translated_key.nil?
+		             country.states.each do | key , value| 
+	                      int_size_official = is_official_name_match(value.name , state_name)
+	                      int_size_unofficial = is_unofficial_name(value.unofficial_names, state_name)
+	                      max_int_size = [int_size_official , int_size_unofficial].max
+	                      if max_int_size > max_count 
+	                      	final_key = key
+	                      	max_count = max_int_size
+	                      end
+		             end
+		             return final_key if max_count > 0
+		             nil
+				end
+
+				private
+
+				def is_unofficial_name(name, state_name)
+					is_array = (name.class == Array) ? true : false
+					return 0 if (!is_array && name.nil?) || (is_array && name.empty?)
+					name_array = name.split(' ').map{|nm| nm.upcase} unless is_array
+				   	name_array = name.map{|nm| nm.upcase} if is_array
+			        state_name_array = state_name.split(' ').map{|nm| nm.upcase}
+			        intersection_size = (name_array & state_name_array).size
+			        intersection_size
+			   end
+
+			   def is_official_name_match(name , state_name)
+			   	    return 0 if name.nil? || state_name.nil?
+			   	    name_array = name.split(' ').map{|nm| nm.upcase}
+			   	    state_name_array = state_name.split(' ').map{|nm| nm.upcase}
+			   	    intersection_size = (name_array & state_name_array).size
+			   	    intersection_size
+			   end
+
+			   def is_translation_match(state_name , country_code)
+			   	   country = ISO3166::Country.new(country_code)
+			   	   country.states.each do |key , subdivision|
+			   	   	subdivision.translations.each do |trans_key , value|
+			   	   		return key if value == state_name
+			   	   	end
+			   	   end
+			   	   nil
+			   end
+		end
+		end
+		end
